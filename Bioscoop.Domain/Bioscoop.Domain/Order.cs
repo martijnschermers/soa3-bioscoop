2929f3aea139a0e2c7d81e9c16877bf24148c510
martijnschermers <martijnschermers2@gmail.com>
2024-01-30 13:54:59 +0100

    feat: add Order class

---
+++ Bioscoop.Domain/Bioscoop.Domain/Order.cs
@@ -0,0 +1,34 @@
+﻿namespace Bioscoop.Domain
+{
+    public class Order
+    {
+        private readonly int _orderNr;
+        private readonly bool _isStudentOrder;
+
+        public Order(int OrderNr, bool IsStudentOrder)
+        {
+            _orderNr = OrderNr;
+            _isStudentOrder = IsStudentOrder;
+        }
+
+        public int GetOrderNr()
+        {
+            return _orderNr;
+        }
+
+        public void AddSeatReservation(MovieTicket movieTicket)
+        {
+            //TODO
+        }
+
+        public double CalculatePrice()
+        {
+            // TODO deel 1
+        }
+
+        public void Export(TicketExportFormat exportFormat)
+        {
+
+        }
+    }
+}
