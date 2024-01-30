e88cb097d320568303842280273a2f44d7d8f502
Luca.R04 <Lucarinck04@gmail.com>
2024-01-30 14:06:39 +0100

    Create MovieTicket.cs

---
+++ Bioscoop.Domain/Bioscoop.Domain/MovieTicket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bioscoop.Domain
+{
+    public class MovieTicket(MovieScreening movieScreening, bool isPremiumReservation, int seatRow, int seatNr)
+    {
+        private int rowNr = seatRow;
+        private int seatNr = seatNr;
+        private bool isPremium = isPremiumReservation;
+
+        public bool isPremiumTicket() { return isPremium; }
+
+        public double getPrice() { return 0; }
+
+        public string toString() { return ""; }
+
+    }
+}
